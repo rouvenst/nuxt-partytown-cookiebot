47a57ad7c44bedcb7eb216b6635a3493a143b3ac
Rouven Stecker <rouven.stecker@gmail.com>
2023-03-22 09:58:57 +0100

    add partytown config (forward and resolveUrl)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    partytown: {
+        forward: ["Cookiebot"],
+        resolveUrl: "function (url, location, type) {\n" +
+            "    if (type === 'script' && (url.href.startsWith('https://consent.cookiebot.com') || url.href.startsWith('https://consentcdn.cookiebot.com'))) {\n" +
+            "      var proxyUrl = new URL('http://127.0.0.1:8788/api/cookiebotproxy');\n" +
+            "      proxyUrl.searchParams.append('url', url.href);\n" +
+            "      return proxyUrl;\n" +
+            "    }\n" +
+            "    return url;\n" +
+            "  }"
+    }, 
 })
